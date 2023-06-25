6863dd34fbb21c5d45f0cee9da063cc7f2c94593
Anand <kumaranand1052000@gmail.com>
2023-06-25 14:11:40 +0530

    first commit

---
+++ db/connect.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+
+const connectDB = (url) => {
+
+    // console.log("DB successfully connected");
+
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports = connectDB;
